3f7ed4fd504953e6d7cbd16b1d030f87eafd65f0
Le Nhat Minh <lenhatminh11a1@gmail.com>
2021-08-16 11:03:19 +0700

    adding dummy data and add form

--- src/pages/AllQuotes.js
+++ src/pages/AllQuotes.js
@@ -1,7 +1,14 @@
 import React from "react";
+import QuoteList from "../components/quotes/QuoteList";
+
+const DUMMY_QUOTES = [
+  { id: "q1", author: "John", text: "Learning React" },
+  { id: "q2", author: "David", text: "Learning Vue" },
+  { id: "q3", author: "Max", text: "Learning Angular" },
+];
 
 const AllQuotes = () => {
-  return <h1>All Quotes Page</h1>;
+  return <QuoteList quotes={DUMMY_QUOTES} />;
 };
 
 export default AllQuotes;
