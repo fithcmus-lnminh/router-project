418bab3458cf515f0e6c8dd394273f861d69d241
Le Nhat Minh <lenhatminh11a1@gmail.com>
2021-08-16 22:45:16 +0700

    adding not found page

---
+++ src/pages/NotFound.js
@@ -0,0 +1,12 @@
+import React from "react";
+import NoQuotesFound from "../components/quotes/NoQuotesFound";
+
+const NotFound = () => {
+  return (
+    <div className="centered">
+      <h1>Page not found!</h1>
+    </div>
+  );
+};
+
+export default NotFound;
