6ce9bf196713f238398ad850a395edc411703d98
Le Nhat Minh <lenhatminh11a1@gmail.com>
2021-08-17 11:01:24 +0700

    using useHistory to nagivate

--- src/pages/NewQuote.js
+++ src/pages/NewQuote.js
@@ -1,9 +1,13 @@
 import React from "react";
 import QuoteForm from "../components/quotes/QuoteForm";
+import { useHistory } from "react-router";
 
 const NewQuote = () => {
+  const history = useHistory(); // history all pages have visited
+
   const addQuoteHandler = (quote) => {
     //add quote
+    history.push("/quotes");
   };
 
   return <QuoteForm onAddQuote={addQuoteHandler} />;
